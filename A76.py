44110e33762e3b8a648ce61d5b12738aa3ba2b07
Z-thrones <zaviyar.ktk@gmail.com>
2026-07-01 13:33:30 +0500

    Add files via upload

---
+++ A76.py
@@ -0,0 +1,8 @@
+setx = {'blue' , 'green'}
+sety = {'blue' , 'yellow'}
+print("The original sets are:/n")
+print(setx)
+print(sety)
+setz = setx.intersection(sety)
+print("The intersected elemnt is /n")
+print(setz)
